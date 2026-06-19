94e67b7b483c20afb6f4c558403c2d224e9ba5a5
taylorjamesbusiness <taylorjamesbusiness0@gmail.com>
2026-06-20 00:02:43 +0600

    Change API endpoint for invoice creation

--- supabase/functions/invoice-create/index.ts
+++ supabase/functions/invoice-create/index.ts
@@ -116,7 +116,7 @@ Deno.serve(async (req) => {
 
 
     // Persist invoice to Supabase
-    const dbRes = await fetch(`${SUPABASE_URL}/rest/v1/invoices`, {
+    const dbRes = await fetch(`${SUPABASE_URL}/rest/v1/invoice-create`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
